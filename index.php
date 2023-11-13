23268b851f8ff468243c06b2886c565e2ceff86b
Loecki03 <luis.loeck@myhsba.de>
2023-11-13 18:53:42 +0100

    Footer fertig gestellt

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
                 <h3>2 Autos mieten zum Preis von 3</h3>             <!--Floating Headerzeile -->
             </div>
         </header>
+
         <header>                                                    <!--Nav Header-->
             <a href="index.php" class="boximg"><img class="logo" src="Bilder/LogoAuto.PNG" alt="logo" width=100px height=100px></a>   <!--Logo-->
             <nav id="f1">
@@ -38,8 +39,61 @@
             </div>
         </div>
 
-        <footer>
-           
+        <footer class="footer">
+           <div class="footer_container">
+                <div class="footer-row">
+                    <div class="footer-col">
+                        <h4>Unternehmen</h4>
+                        <ul>
+                            <li><a href="#">Impressum</a></li>
+                            <li><a href="#">AGB</a></li>
+                            <li><a href="#">Datenschutz</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Über uns</h4>
+                        <ul>
+                            <li><a href="#">Kontakt</a></li>
+                            <li><a href="#">Affiliate Program</a></li>
+                            <li><a href="#">Über uns</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Hilfe</h4>
+                        <ul>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Folg uns</h4>
+                        <div class="social-links">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        </footer>
+
+        <footer class="footer2">
+            <div class="pay-text">
+                <h4>Zahlungsmethoden</h4>  
+            </div>
+            <div class="pay-img"> 
+                <p><img src="Bilder/apple.png"></p>
+                <p><img src="Bilder/googlepay.png"></p>
+                <p><img src="Bilder/klarna.png"></p>
+                <p><img src="Bilder/mastercard.png"></p>
+                <p><img src="Bilder/visa.png"></p>
+                <p><img src="Bilder/paypal.png"></p>
+            </div>
+            <div class="stylep">
+                <p>&copy; 2023 CarSBA. Alle Rechte vorbehalten.</p>
+            </div>
         </footer>
         
     </body>
