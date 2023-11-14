677e098d468200cbbbc483acaa3dce178d64cc8b
gerion62 <150141893+gerion62@users.noreply.github.com>
2023-11-14 11:00:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,32 +2,32 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">       
-        <title></title>                                                         <!-- Standart HTML Settings -->
+        <title></title>                                                                         <!-- Standart HTML Settings -->
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="homepage.css">                             <!--CSS Datei Import -->
+        <link rel="stylesheet" href="homepage.css">                                             <!--CSS Datei Import -->
     </head>
 <!-- Beginn der Webseite -->
     <body>
         <header id="float">
             <div id="band" class="">
-                <h3>2 Autos mieten zum Preis von 3</h3>             <!--Floating Headerzeile -->
+                <h3>2 Autos mieten zum Preis von 3</h3>                                         <!--Floating Headerzeile -->
             </div>
         </header>
 
-        <header>                                                    <!--Nav Header-->
+        <header>                                                                                <!--Nav Header-->
             <a href="index.php" class="boximg"><img class="logo" src="Bilder/LogoAuto.PNG" alt="logo" width=100px height=100px></a>   <!--Logo-->
             <nav id="f1">
                 <ul class="nav__links">
                     <li class="boxtext"><a href="#">Fahrzeuge</a></li>
-                    <li class="boxtext"><a href="#">Standorte</a></li>              <!--Navigation Links erstellt-->
-                    <li class="boxtext"><a href="#">Mieten</a></li>
+                    <li class="boxtext"><a href="#">Standorte</a></li>              
+                    <li class="boxtext"><a href="#">Mieten</a></li>                             <!--Navigation Links erstellt--> 
                     <li class="boxtext"><a href="#">Kontakt</a></li>
                 </ul>
             </nav>
             <nav id="autoleft">
                 <ul class="nav__links">
-                    <li class="boxtext"><a href="#">Meine Buchungen</a></li>            <!--Meine Buchungen + Login erstellt-->
+                    <li class="boxtext"><a href="#">Meine Buchungen</a></li>                    <!--Meine Buchungen + Login erstellt-->
                     <li class="log"><a href="#"><button>Login</button></a></li>
                 </ul>
             </nav>
@@ -42,8 +42,8 @@
 
 
 
-        <!-- Google Maps -->
-     <!-- Darstellung einer Karte von Google Maps, wodurch man zum vordefinierten Standort navigieren kann. -->
+                                                                                                <!-- Google Maps -->
+                                                                                                <!-- Darstellung einer Karte von Google Maps, wodurch man zum vordefinierten Standort navigieren kann. -->
     <div class="googlemaps">
          <h2 class="subtitle">Unser Hauptsitz</h2>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4521.810281179028!2d9.983530443398028!3d53.54759672432389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18f052906e573%3A0x91f76f669af7ad22!2sWilly-Brandt-Stra%C3%9Fe%2075%2C%2020459%20Hamburg!5e0!3m2!1sde!2sde!4v1621458625556!5m2!1sde!2sde"width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy">
