58e1e740b0d242b40893183aa136695ca7bc31e4
DankoRother <131434063+DankoRother@users.noreply.github.com>
2023-11-14 13:11:58 +0000

    Impressum isst dort!

--- impressum.php
+++ impressum.php
@@ -5,29 +5,60 @@
         <title></title>                                                         <!-- Standart HTML Settings -->
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="homepage.css">        
+        <link rel="stylesheet" href="impressum.css">        
+
 </head>
 
 <body style="background-color: #ffffff;">
+<?php include 'header.php'; ?>                                                  <!-- Header-->
 <table width="100%">
     <tr>
-        <td width="33%" style="background-color: #ffffff;"></td>
-        <td width="33$" style="background-color: #24252a;">
-            <div align="center" style="text-align: center;">
+       <!-- <td width="10%" style="background-color: #ffffff;"></td> -->
+        <td width="50%" style="background-color: #24252a;">
+            <div align="center" class="dividiediv">
+            <br>
+            <br>
                 <h1>Impressum</h1>
                 <br>
-                <br>
-
-                <p class="impressum">
-                    CarSBA GmbH <br>
-                    Am Krummen Deich 18 <br>
-                    21423 Winsen Luhe <br>
+                <h2> CarSBA GmbH </h2>
+                <h3> Willy-Brandt Straße 75 <br>
+                 20459 Hamburg </h3>
                 </p> 
+                <br>
+                <br>
+                <h3> 
+                E-Mail: gerion.gutzeit@myhsba.de <br>
+                Tel: (+49) 0171 4411144 
+                <br>
+                <br>
+                vertreten durch den Vorstand: <br>
+                Luis Loeck, Vorsitzender<br>
+                Linus Karanikas, stv. Vorsitzender<br>
+                Jason Wagner <br>
+                Gerion Gutzeit <br>
+                Danko Rother <br>
+                <br>
+                <br>
+                Registergericht: Amtsgericht Hamburg <br>
+                HRB-Nr. 21423187 <br>
+                USt.-Ident.-Nr. DE 112 214 187 <br><br><br>
+                </h3>
+                <h2>Verbraucherinformation gemäß Verordnung EU Nr. 524/2013</h2>
+                <h3>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit. Die Plattform finden Sie unter http://ec.europa.eu/consumers/odr/ <br><br>
+                Unsere Email-Adresse lautet: info@carsba.de </h3><br><br>
+                <h2>Verbraucherinformation gemäß Verbraucherstreitbeilegungsgesetz</h2><br>
+                <h3>Die CarSBA GmbH ist nicht bereit und verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</h3><br><br>
+                <h2>Rechtlicher Hinweis</h2><br>
+                <h3>Die Texte, Bilder, Grafiken, Animationen, Video- und Audiodateien sowie alle weiteren Inhalte auf dieser Webseite unterliegen den gesetzlichen Bestimmungen des Urheberrechts und ggf. weiteren Rechten des geistigen Eigentums. Der geschützte Inhalt dieser Webseite darf ohne Einwilligung des Rechtsinhabers nicht vervielfältigt, verbreitet, wiedergegeben, öffentlich zugänglich gemacht oder sonst verwertet werden.
+                Änderungen von Konstruktion, Ausstattung und Lieferumfang sowie Abweichungen im Farbton sowie Irrtümer bleiben vorbehalten. Alle angegebenen Preise sind unverbindlich.</h3>
+                <br>
+                <br>
             </div>
         </td>
-            <td width="33%" style="background-color: #ffffff;"></td>
+           <!-- <td width="10%" style="background-color: #ffffff;"></td>-->
     </tr>
 </table>
+<?php include 'footer.php'; ?>      
 </body>
 
 </html>
