661fffe0aea22478fadedf6ffd0a97c8ddcd3f40
Loecki03 <luis.loeck@myhsba.de>
2023-11-14 12:12:38 +0100

    Header und footer seperat

--- index.php
+++ index.php
@@ -9,30 +9,7 @@
     </head>
 <!-- Beginn der Webseite -->
     <body>
-        <header id="float">
-            <div id="band" class="">
-                <h3>2 Autos mieten zum Preis von 3</h3>                                         <!--Floating Headerzeile -->
-            </div>
-        </header>
-
-        <header>                                                                                <!--Nav Header-->
-            <a href="index.php" class="boximg"><img class="logo" src="Bilder/LogoAuto.PNG" alt="logo" width=100px height=100px></a>   <!--Logo-->
-            <nav id="f1">
-                <ul class="nav__links">
-                    <li class="boxtext"><a href="#">Fahrzeuge</a></li>
-                    <li class="boxtext"><a href="#">Standorte</a></li>              
-                    <li class="boxtext"><a href="#">Mieten</a></li>                             <!--Navigation Links erstellt--> 
-                    <li class="boxtext"><a href="#">Kontakt</a></li>
-                </ul>
-            </nav>
-            <nav id="autoleft">
-                <ul class="nav__links">
-                    <li class="boxtext"><a href="#">Meine Buchungen</a></li>         <!--Meine Buchungen + Login erstellt-->
-                    <li class="boxtext"><a href="#">Meine Buchungen</a></li>                    <!--Meine Buchungen + Login erstellt-->
-                    <li class="log"><a href="#"><button>Login</button></a></li>
-                </ul>
-            </nav>
-        </header>
+        <?php include 'header.php'; ?>
 
         <div class="image">
             <div class="image-overlay">
@@ -51,66 +28,7 @@
          </iframe>
     </div>
 
-
-
-
-
-        <footer class="footer">
-           <div class="footer_container">
-                <div class="footer-row">
-                    <div class="footer-col">
-                        <h4>Unternehmen</h4>
-                        <ul>
-                            <li><a href="#">Impressum</a></li>
-                            <li><a href="#">AGB</a></li>
-                            <li><a href="#">Datenschutz</a></li>
-                        </ul>
-                    </div>
-                    <div class="footer-col">
-                        <h4>Über uns</h4>
-                        <ul>
-                            <li><a href="#">Kontakt</a></li>
-                            <li><a href="#">Affiliate Program</a></li>
-                            <li><a href="#">Über uns</a></li>
-                        </ul>
-                    </div>
-                    <div class="footer-col">
-                        <h4>Hilfe</h4>
-                        <ul>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">Test</a></li>
-                            <li><a href="#">Test</a></li>
-                        </ul>
-                    </div>
-                    <div class="footer-col">
-                        <h4>Folg uns</h4>
-                        <div class="social-links">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                        </div>
-                    </div>
-                </div>
-           </div>
-        </footer>
-
-        <footer class="footer2">
-            <div class="pay-text">
-                <h4>Zahlungsmethoden</h4>  
-            </div>
-            <div class="pay-img"> 
-                <p><img src="Bilder/apple.png"></p>
-                <p><img src="Bilder/googlepay.png"></p>
-                <p><img src="Bilder/klarna.png"></p>
-                <p><img src="Bilder/mastercard.png"></p>
-                <p><img src="Bilder/visa.png"></p>
-                <p><img src="Bilder/paypal.png"></p>
-            </div>
-            <div class="stylep">
-                <p>&copy; 2023 CarSBA. Alle Rechte vorbehalten.</p>
-            </div>
-        </footer>
+    <?php include 'footer.php'; ?>
         
     </body>
 
