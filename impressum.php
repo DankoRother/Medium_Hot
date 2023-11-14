9eec2744dee48286c545198cf6611b1487e53515
DankoRother <131434063+DankoRother@users.noreply.github.com>
2023-11-14 12:23:00 +0000

    Impressum Begin

--- impressum.php
+++ impressum.php
@@ -1,7 +1,33 @@
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" type="text/css" href="homepage.css">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">       
+        <title></title>                                                         <!-- Standart HTML Settings -->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="homepage.css">        
 </head>
-<body>
+
+<body style="background-color: #ffffff;">
+<table width="100%">
+    <tr>
+        <td width="33%" style="background-color: #ffffff;"></td>
+        <td width="33$" style="background-color: #24252a;">
+            <div align="center" style="text-align: center;">
+                <h1>Impressum</h1>
+                <br>
+                <br>
+
+                <p class="impressum">
+                    CarSBA GmbH <br>
+                    Am Krummen Deich 18 <br>
+                    21423 Winsen Luhe <br>
+                </p> 
+            </div>
+        </td>
+            <td width="33%" style="background-color: #ffffff;"></td>
+    </tr>
+</table>
+</body>
 
 </html>
