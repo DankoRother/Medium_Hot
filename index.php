e7fe580178ec2c5fbaeb71275b89e99bc1886079
gerion62 <150141893+gerion62@users.noreply.github.com>
2023-11-14 12:50:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,13 +19,15 @@
 
 
         <div class="divheading2">                                                               
-            <p class="headinggooglemaps">Unser Hauptsitz</p>
+            <p class="mainheading2">
+                Unser Hauptsitz
+            </p>
         </div>
 
-         <div class="googlemaps">                                                               <!-- Einfügen der Karte von Google Maps -->
+        <div class="googlemaps">                                                               <!-- Einfügen der Karte von Google Maps -->
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4521.810281179028!2d9.983530443398028!3d53.54759672432389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18f052906e573%3A0x91f76f669af7ad22!2sWilly-Brandt-Stra%C3%9Fe%2075%2C%2020459%20Hamburg!5e0!3m2!1sde!2sde!4v1621458625556!5m2!1sde!2sde"width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy">
-         </iframe>
-    </div>
+            </iframe>
+        </div>
 
     <?php include 'footer.php'; ?>
         
