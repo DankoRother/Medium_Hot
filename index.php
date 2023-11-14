95331ef3ec63a09034f86c7bec488d04bbbd73f2
gerion62 <150141893+gerion62@users.noreply.github.com>
2023-11-14 12:44:53 +0100

    anpassung google maps

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <title></title>                                                                         <!-- Standart HTML Settings -->
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="homepage.css">                                             <!--CSS Datei Import -->
+        <link rel="stylesheet" href="homepage.css">                                             <!-- CSS Datei Import -->
     </head>
 <!-- Beginn der Webseite -->
     <body>
@@ -18,15 +18,11 @@
         </div>
 
 
-
-
-                                                                                                <!-- Google Maps -->
-                                                                                                <!-- Darstellung einer Karte von Google Maps, wodurch man zum vordefinierten Standort navigieren kann. -->
-        <div class="divheading2">
+        <div class="divheading2">                                                               
             <p class="headinggooglemaps">Unser Hauptsitz</p>
         </div>
 
-         <div class="googlemaps">
+         <div class="googlemaps">                                                               <!-- Einfügen der Karte von Google Maps -->
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4521.810281179028!2d9.983530443398028!3d53.54759672432389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18f052906e573%3A0x91f76f669af7ad22!2sWilly-Brandt-Stra%C3%9Fe%2075%2C%2020459%20Hamburg!5e0!3m2!1sde!2sde!4v1621458625556!5m2!1sde!2sde"width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy">
          </iframe>
     </div>
