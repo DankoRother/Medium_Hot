dbdbf899c0e1535ddfebeb108e9e013cb312d4f9
DankoRother <131434063+DankoRother@users.noreply.github.com>
2023-11-15 12:52:49 +0000

    Login, AGB und Impressum

---
+++ agb.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">       
+        <title></title>                                                         <!-- Standart HTML Settings -->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="impressum.css">        
+
+</head>
+
+<body style="background-color: #ffffff;">
+<?php include 'header.php'; ?>                                                  <!-- Header-->
+<table width="100%">
+    <tr>
+       <!-- <td width="10%" style="background-color: #ffffff;"></td> -->
+        <td width="50%" style="background-color: #24252a;">
+            <div align="center" class="dividiediv">
+                <h1>Allgemeine Geschäftsbedingungen</h1>
+                
+            </div>
+        </td>
+           <!-- <td width="10%" style="background-color: #ffffff;"></td>-->
+    </tr>
+</table>
+<?php include 'footer.php'; ?>      
+</body>
+
+</html>
