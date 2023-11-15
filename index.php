7140bbe57d0a5827c5a39ccb7aa8d4abd04805ea
Loecki03 <luis.loeck@myhsba.de>
2023-11-15 13:08:44 +0100

    Test team

--- index.php
+++ index.php
@@ -17,68 +17,6 @@
             </div>
         </div>
 
-    <div class="div6">
-        <h3 class="headingforcontact">Wer wir sind</h3>
-      <div class="contact">
-        <div class="contact-box">
-          <div>
-            <img src="Bilder/lambo.png" alt="Profilfoto">
-          </div>
-          <ul>
-            <li><b>Jason Wagner</b></li>
-            <li class="li_space">CEO - Chief Executive Officer</li>
-            <li><p>jason.wagner<br>@carsba.de</p></li>
-          </ul>
-        </div>
-
-        <div class="contact-box">
-          <div>
-            <img src="Bilder/lambo.png" alt="Profilfoto">
-          </div>
-          <ul>
-            <li><b>Luis Loeck</b></li>
-            <li class="li_space">CFO - Chief Financial Officer</li>
-            <li><p>luis.loeck<br>@carsba.de</p></li>
-          </ul>
-        </div>
-
-        <div class="contact-box">
-          <div>
-            <img src="Bilder/lambo.png" alt="Profilfoto">
-          </div>
-          <ul>
-            <li><b>Danko Rother</b></li>
-            <li class="li_space">CMO - Chief Marketing Officer</li>
-            <li><p>danko.rother<br>@carsba.de</p></li>
-          </ul>
-        </div>
-
-        <div class="contact-box">
-          <div>
-            <img src="Bilder/lambo.png" alt="Profilfoto">
-          </div>
-          <ul>
-            <li><b>Gerion Gutzeit</b></li>
-            <li class="li_space">CBDO - Chief Business Development Officer</li>
-            <li><p>gerion.gutzeit<br>@carsba.de</p></li>
-          </ul>
-        </div>
-
-        <div class="contact-box">
-          <div>
-            <img src="Bilder/lambo.png" alt="Profilfoto">
-          </div>
-          <ul>
-            <li><b>Linus Karanikas</b></li>
-            <li class="li_space">CHRO - Chief Human Resources Officer</li>
-            <li><p>linus.karanikas <br>@carsba.de</p></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    </div>
-
-
         <div class="divheading2">                                                               
             <p class="mainheading2">Unser Hauptsitz</p>
         </div>
@@ -86,6 +24,28 @@
         <div class="googlemaps">                                                               <!-- Einfügen der Karte von Google Maps -->
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4521.810281179028!2d9.983530443398028!3d53.54759672432389!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18f052906e573%3A0x91f76f669af7ad22!2sWilly-Brandt-Stra%C3%9Fe%2075%2C%2020459%20Hamburg!5e0!3m2!1sde!2sde!4v1621458625556!5m2!1sde!2sde"width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy">
             </iframe>
+        </div>
+
+        <div class="headdiv"><h1 align="center">Unser Team</h1></div>
+        <div class="container">
+            <div class="small-container">
+                <div class="member">
+                    <img src="Bilder/luis.JPG">
+                    <h3>Luis Loeck</h3>
+                </div>
+                <div class="member">
+                    <img src="Bilder/luis.JPG">
+                </div>
+                <div class="member">
+                    <img src="Bilder/luis.JPG">
+                </div>
+                <div class="member">
+                    <img src="Bilder/luis.JPG">
+                </div>
+                <div class="member">
+                    <img src="Bilder/luis.JPG">
+                </div>
+            </div>
         </div>
 
     <?php include 'footer.php'; ?>
