67ee6f091382d80a6087f6f37645a8eeb1cbcdbb
Loecki03 <luis.loeck@myhsba.de>
2023-11-18 21:25:00 +0100

    agb und datenschutz fertig

--- agb.php
+++ agb.php
@@ -28,21 +28,21 @@
                 <h3>1. Vertragsgegenstand</h3>
                 <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p><br>
                 <h3>2. Mietdauer</h3>
-                <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p><br>
+                <p>2.1 Die Mietdauer beginnt und endet gemäß den im Vertrag festgelegten Daten und Uhrzeiten.<br>2.2 Eine Verlängerung der Mietdauer bedarf der Zustimmung von CarSBA.</p><br>
                 <h3>3. Mietpreis und Zahlungsbedingungen</h3>
-                <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p><br>
+                <p>3.1 Der Mieter verpflichtet sich, den vereinbarten Mietpreis gemäß den im Vertrag festgelegten Zahlungsbedingungen zu entrichten.<br>3.2 Zusätzliche Kosten, wie Treibstoff, Mautgebühren und Strafzettel, gehen zu Lasten des Mieters.</p><br>
                 <h3>4. Fahrzeugnutzung und -pflege</h3>
-                <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p><br>
+                <p>4.1 Der Mieter verpflichtet sich, das Fahrzeug sachgemäß zu nutzen und in einem ordnungsgemäßen Zustand zu halten.<br>4.2 Jegliche Schäden oder Mängel am Fahrzeug sind CarSBA unverzüglich zu melden.</p><br>
                 <h3>5. Versicherung</h3>
-                <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p><br>
+                <p>5.1 Das Fahrzeug ist gemäß den gesetzlichen Bestimmungen versichert.<br>5.2 Der Mieter ist verpflichtet, Schäden oder Unfälle dem CarSBA und den örtlichen Behörden unverzüglich zu melden.</p><br>
                 <h3>6. Haftung</h3>
-                <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p><br>
+                <p>6.1 CarSBA haftet nicht für Verluste oder Schäden an persönlichem Eigentum des Mieters im Fahrzeug.<br>6.2 Der Mieter haftet für Schäden am Fahrzeug, die während der Mietdauer entstehen.</p><br>
                 <h3>7. Rückgabe des Fahrzeugs</h3>
-                <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p><br>
+                <p>7.1 Das Fahrzeug ist am Ende der Mietdauer in dem Zustand zurückzugeben, in dem es übernommen wurde.<br>7.2 Verspätete Rückgaben können zusätzliche Gebühren nach sich ziehen.</p><br>
                 <h3>8. Stornierung</h3>
-                <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p><br>
+                <p>8.1 Stornierungen müssen gemäß den im Vertrag festgelegten Bedingungen erfolgen.<br>8.2 Bei verspäteter Stornierung können Stornogebühren anfallen.</p><br>
                 <h3>9. Sonstige Bestimmungen</h3>
-                <p>1.1 CarSBA stellt dem Mieter das im Vertrag näher beschriebene Fahrzeug zur Verfügung.<br>1.2 Das Fahrzeug bleibt Eigentum von CarSBA.</p>
+                <p>9.1 Änderungen oder Ergänzungen des Vertrags bedürfen der Schriftform.<br>9.2 Gerichtsstand für alle Streitigkeiten aus diesem Vertrag ist der Sitz von CarSBA.</p>
             </div>
         </td>
     </tr>
--- datenschutz.php
+++ datenschutz.php
@@ -16,8 +16,33 @@
        <!-- <td width="10%" style="background-color: #ffffff;"></td> -->
         <td width="50%" style="background-color: #24252a;">
             <div align="center" class="dividiediv">
-                <h1>Datenschutz</h1>
-                
+                <h1>Datenschutz</h1><br>
+                <p>Die Autovermietung "CarSBA" nimmt den Schutz Ihrer personenbezogenen Daten ernst. 
+                Nachfolgend möchten wir Sie darüber informieren, welche Daten wir erheben, wie wir sie verwenden und schützen.</p><br>
+                <h3>1. Erhebung und Verarbeitung personenbezogener Daten</h3><br>
+                <p>Wir erheben und verarbeiten personenbezogene Daten, um Ihnen unsere Fahrzeuge zur Verfügung zu stellen. 
+                Dazu gehören Informationen wie Ihr Name, Ihre Adresse, Kontaktdaten, Führerscheininformationen und Zahlungsinformationen.</p><br>
+                <h3>2. Nutzung personenbezogener Daten</h3><br>
+                <p>Die von uns erfassten Daten dienen der Vertragsabwicklung, der Bereitstellung von Fahrzeugen und der Abrechnung. 
+                    Gelegentlich nutzen wir diese Informationen auch für Marketingzwecke, um Sie über Angebote und Dienstleistungen zu informieren. 
+                    Sie haben das Recht, dieser Verwendung Ihrer Daten zu widersprechen.</p><br>
+                <h3>3. Weitergabe von Daten an Dritte</h3><br>
+                <p>Personenbezogene Daten werden nur an Dritte weitergegeben, wenn dies zur Vertragserfüllung erforderlich ist oder gesetzlich vorgeschrieben ist. 
+                    Dritte, die Zugang zu Ihren Daten erhalten, sind verpflichtet, die Datenschutzbestimmungen einzuhalten.</p><br>
+                <h3>4. Datensicherheit</h3><br>
+                <p>Wir setzen angemessene technische und organisatorische Maßnahmen ein, um die Sicherheit Ihrer Daten zu gewährleisten. 
+                    Kundenkonten sind durch Passwörter geschützt, und Zahlungsinformationen werden verschlüsselt übertragen.</p><br>
+                <h3>5. Auskunftsrecht und Berichtigung</h3><br>
+                <p>Sie haben das Recht, Auskunft über die von uns gespeicherten Daten zu erhalten. 
+                    Falls Daten unrichtig oder unvollständig sind, werden wir diese auf Ihre Anfrage hin korrigieren oder löschen.</p><br>
+                <h3>6. Cookies und Tracking-Technologien</h3><br>
+                <p>Wir verwenden Cookies und ähnliche Technologien, um die Nutzung unserer Website zu analysieren und Ihr Benutzererlebnis zu verbessern. 
+                    Sie können die Verwendung von Cookies in Ihren Browsereinstellungen kontrollieren.</p><br>
+                <h3>7. Änderungen der Datenschutzerklärung</h3><br>
+                <p>Wir behalten uns das Recht vor, diese Datenschutzerklärung bei Bedarf zu aktualisieren. Wesentliche Änderungen werden wir Ihnen mitteilen.</p><br>
+                <h3>8. Kontaktinformationen</h3><br>
+                <p>Für Fragen zur Datenschutzerklärung oder zur Ausübung Ihrer Datenschutzrechte können Sie sich an uns wenden:</p><br>
+                <p>CarSBA GmbH<br>Willy-Brandt-Straße 75<br>20459 Hamburg<br>Kontakt: info@carsba.de</p>
             </div>
         </td>
            <!-- <td width="10%" style="background-color: #ffffff;"></td>-->
