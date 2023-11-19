51b0c7364e285ea8f88a923d58592f26b2bf7abd
Loecki03 <luis.loeck@myhsba.de>
2023-11-19 01:56:45 +0100

    standort seite hinzugefügt

--- standorte.php
+++ standorte.php
@@ -5,19 +5,28 @@
         <title>Standorte</title>                                                                         <!-- Standart HTML Settings -->
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="CSS/homepage.css">                                             <!-- CSS Datei Import -->
+        <link rel="stylesheet" href="CSS/standorte.css">                                             <!-- CSS Datei Import -->
     </head>
 
     <body>
     <?php include 'header.php'; ?>
-
-    <div class="ger-map">
-        <img src="" alt="map" >
-        <div class="pin hamburg">
-            <span>Hamburg</span>
+    <div class="map-heading">
+        <h1>Unsere Standorte</h1>
+    </div>
+    <div class="map-container">
+        <div class="ger-map">
+            <img src="Bilder/karte.png" alt="map" >
+            <div class="pin hamburg">
+                <span>Hamburg</span>
+            </div>
+            <div class="pin berlin">
+                <span>Berlin</span>
+            </div>
+            <div class="pin hannover">
+                <span>Hannover</span>
+            </div>
         </div>
     </div>
-
 
 
     <?php include 'footer.php'; ?>
