5d62c520d3fd70d1d755ae75443d7ed48662b2fa
Loecki03 <luis.loeck@myhsba.de>
2023-11-23 13:56:05 +0100

    mieten filter

--- mieten.php
+++ mieten.php
@@ -12,7 +12,7 @@
 <?php include 'header.php'; ?>
 
 <div class="img-container">
-    
+
 <div class="filter-heading">
     <h2>Unsere Angebote</h2>
 </div>
@@ -60,7 +60,24 @@
 <div class="filter-container-2">
     <div class="suchfilter-extended">
         <form action="" method="GET">
-            
+            <div class="filter-row-2">
+                <div class="filter-bar-2">
+                    <h3>Hersteller</h3>
+                    <select name="ort" class="form-select-2">
+                        <option value="">Audi</option>
+                        <option value="">BMW</option>
+                        <option value="">Volkswagen</option>
+                        <option value="">Mercedes-Benz</option>
+                        <option value="">Ford</option>
+                        <option value="">Range Rover</option>
+                        <option value="">Mercedes-AMG</option>
+                        <option value="">Opel</option>
+                        <option value="">Jaguar</option>
+                        <option value="">Maserati</option>
+                        <option value="">Skoda</option>
+                    </select>
+                </div>
+            </div>
         </form>
     </div>
 </div>
