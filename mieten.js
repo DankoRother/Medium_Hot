09ea5940483187cc1c516ec9ac40bb1d369b8743
Loecki03 <luis.loeck@myhsba.de>
2023-11-30 12:05:58 +0100

    pfeil nach oben und unten switched beim aufklappen

--- mieten.js
+++ mieten.js
@@ -1,13 +1,16 @@
-var chevronIcon = document.getElementById("chevron");
 function show_hide() {
     var x = document.getElementById("filter2");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        chevronIcon.classList.remove('fa-chevron-down');
-        chevronIcon.classList.add('fa-chevron-up');
-    } else {
-        x.style.display = "none";
-        chevronIcon.classList.remove('fa-chevron-up');
-        chevronIcon.classList.add('fa-chevron-down');
+    var chevronIcon = document.getElementById("chevron");
+
+    if (x && chevronIcon) {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+            chevronIcon.classList.remove('fa-chevron-down');
+            chevronIcon.classList.add('fa-chevron-up');
+        } else {
+            x.style.display = "none";
+            chevronIcon.classList.remove('fa-chevron-up');
+            chevronIcon.classList.add('fa-chevron-down');
+        }
     }
 }
