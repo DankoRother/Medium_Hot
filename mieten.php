ca876521264eaae527826ce472632705b490983f
Loecki03 <luis.loeck@myhsba.de>
2023-12-07 22:15:37 +0100

    date fix

--- mieten.php
+++ mieten.php
@@ -1,9 +1,9 @@
 
 <!DOCTYPE html>
     <head>                                 <!-- Verlinkung zu Jquery -->
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>                                    <!-- Verlinkung zu Jquery -->
         <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
         <script>                                                                                              
   $(function() {
     $("#start_date").datepicker({                                                                               //Codeabschnitt von Jquery: Eingabe von Datumswerten unter Berücksichitigung, dass zum Beispiel das Startdatum nicht größer als das Enddatum sein darf.
