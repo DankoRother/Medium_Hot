c5bc666a362b2dece34b5a4472cbd87b88fdd9aa
DankoRother <131434063+DankoRother@users.noreply.github.com>
2023-12-20 23:50:59 +0100

    Comments for login.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php session_start();?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +11,11 @@
 </head>
 <body>
     <?php include 'header.php' ?>
+
     <?php 
+    // Check if a user is already logged in
     if (isset($_SESSION['logged_in_userID']) && $_SESSION['logged_in_userID'] > 0) {
+        // Display a message and redirect to the home page if logged in
         echo('<br>
         <br>
         <br>
@@ -27,11 +31,13 @@
         </div>
         <br>
         <br>');
+        // Redirect to the home page after 2 seconds
         echo('<script> setTimeout(function() {
             window.location.href = "home.php";
             }, 2000); </script>');
     }
     else {
+        // Display the login form if no user is logged in
         echo '
             <br>
             <br>
@@ -41,23 +47,25 @@
             </div>
             <br>
             <div class="roundedcorners" id="roundedcorners">
+                <!-- Login form -->
                 <form method="POST" id="loginForm" onsubmit="loginFormSubmit(event)">
-                <container class="flex">
-                    <input class="input" type="text" id="loginUsername" name="loginUsername" style="order: 4">
-                    <label class="label" for="loginUsername" style="order: 5">Username:</label>
-                    <input class="input" type="text" id="loginPassword" name="loginPassword" style="order: 2">
-                    <label class="label" for="loginPassword" style="order: 3">Passwort:</label>
-                    <input class="login" type="submit" name="submit" value="Anmelden" id="loginButton" style="order: 1" disabled>
-                    <br>
-                    <div class="error" id="error"></div>
-                </container>
-
+                    <container class="flex">
+                        <input class="input" type="text" id="loginUsername" name="loginUsername" style="order: 4">
+                        <label class="label" for="loginUsername" style="order: 5">Username:</label>
+                        <input class="input" type="text" id="loginPassword" name="loginPassword" style="order: 2">
+                        <label class="label" for="loginPassword" style="order: 3">Passwort:</label>
+                        <input class="login" type="submit" name="submit" value="Anmelden" id="loginButton" style="order: 1" disabled>
+                        <br>
+                        <div class="error" id="error"></div>
+                    </container>
                 </form>
             </div>
+            <!-- Link to register page -->
             <a class="login" href="register.php"><button class="login">Registrieren</button></a>';
     }
     ?>
     <?php include 'footer.php' ?>
+    <!-- Include the JavaScript file for handling login interactions -->
     <script src="js/loginscript.js"></script>
 </body>
 </html>
