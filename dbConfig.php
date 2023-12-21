bc4b72ed82e09dc1d81220ed3155e7e902f4b09a
Loecki03 <luis.loeck@myhsba.de>
2023-12-21 12:33:46 +0100

    kommentare dbconfig

--- dbConfig.php
+++ dbConfig.php
@@ -1,13 +1,18 @@
 <?php
+ // Database connection parameters
     $servername = "localhost";
     $username = "testuser23";
     $password = "paswort123!";
     $dbname = "carsba";
 
 try {
+    // Create a new PDO (PHP Data Objects) instance for database connection
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    // Set the PDO attribute to throw exceptions for errors
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
+    // If an exception occurs during the connection, catch and display the error message
     echo "Error: " . $e->getMessage();
 }
 ?>
