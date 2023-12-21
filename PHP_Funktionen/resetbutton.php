d9a349b80564f3ba1c0528bf842f6d16fd8d5fff
Loecki03 <luis.loeck@myhsba.de>
2023-12-21 13:22:18 +0100

    kommentare

--- PHP_Funktionen/resetbutton.php
+++ PHP_Funktionen/resetbutton.php
@@ -1,5 +1,7 @@
 <?php
+// Reset button logic: Reset specific filter options
 if (isset($_POST['resetButton'])) {
+    // Reset individual filter options in the session
     $_SESSION['vendor'] = "";
     $_SESSION['type'] = "";
     $_SESSION['price'] = "";
@@ -13,32 +15,14 @@ if (isset($_POST['resetButton'])) {
     $_SESSION['trunk'] = "";
     $_SESSION['sort'] = "ASC";
 
-  header("Location: {$_SERVER['PHP_SELF']}");
-  exit;
+    // Redirect to the same page to apply the changes
+    header("Location: {$_SERVER['PHP_SELF']}");
+    exit;
 }
 
+// Reset all filters logic: Reset all filter options and return to the home page
 if (isset($_POST['resetAll'])) {
-    $_SESSION['start_date'] = date('m/d/Y');;
-    $_SESSION['end_date'] = date('m/d/Y', strtotime('+1 day'));
-    $_SESSION['location'] = "";
-    $_SESSION['vendor'] = "";
-    $_SESSION['type'] = "";
-    $_SESSION['price'] = "";
-    $_SESSION['seats'] = "";
-    $_SESSION['gear'] = "";
-    $_SESSION['doors'] = "";
-    $_SESSION['min_age'] = "";
-    $_SESSION['drive'] = "";
-    $_SESSION['gps'] = "";
-    $_SESSION['air_condition'] = "";
-    $_SESSION['trunk'] = "";
-    $_SESSION['sort'] = "ASC";
-
-  header("Location: home.php");
-  exit;
-}
-
-if (isset($_POST['resetHome'])) {
+    // Reset all filter options in the session
     $_SESSION['start_date'] = date('m/d/Y');
     $_SESSION['end_date'] = date('m/d/Y', strtotime('+1 day'));
     $_SESSION['location'] = "";
@@ -55,8 +39,32 @@ if (isset($_POST['resetHome'])) {
     $_SESSION['trunk'] = "";
     $_SESSION['sort'] = "ASC";
 
-header("Location: home.php");
-exit;
+    // Redirect to the home page to apply the changes
+    header("Location: home.php");
+    exit;
 }
 
+// Reset home button logic: Reset filters and return to the home page
+if (isset($_POST['resetHome'])) {
+    // Reset filters in the session
+    $_SESSION['start_date'] = date('m/d/Y');
+    $_SESSION['end_date'] = date('m/d/Y', strtotime('+1 day'));
+    $_SESSION['location'] = "";
+    $_SESSION['vendor'] = "";
+    $_SESSION['type'] = "";
+    $_SESSION['price'] = "";
+    $_SESSION['seats'] = "";
+    $_SESSION['gear'] = "";
+    $_SESSION['doors'] = "";
+    $_SESSION['min_age'] = "";
+    $_SESSION['drive'] = "";
+    $_SESSION['gps'] = "";
+    $_SESSION['air_condition'] = "";
+    $_SESSION['trunk'] = "";
+    $_SESSION['sort'] = "ASC";
+
+    // Redirect to the home page to apply the changes
+    header("Location: home.php");
+    exit;
+}
 ?>
--- PHP_Funktionen/userabfrage.php
+++ PHP_Funktionen/userabfrage.php
@@ -1,20 +1,25 @@
 <?php 
+// Check if the user is logged in
 if (isset($_SESSION['logged_in_userID']) && $_SESSION['logged_in_userID'] > 0) { 
 
+    // Retrieve the user ID from the session
     $userId = $_SESSION['logged_in_userID'];
 
+    // SQL query to select user information based on user ID
     $sqluser = "SELECT *
                 FROM user
                 WHERE user.userId = :selectedUserId";
     
+    // Prepare the SQL statement
     $stmt = $conn->prepare($sqluser);
     
-    // Binden der Parameter
+    // Bind the parameter for the user ID
     $stmt->bindParam(':selectedUserId', $userId, PDO::PARAM_INT);
     
+    // Execute the SQL statement
     $stmt->execute();
     
+    // Fetch the result set into an associative array
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 }
-
 ?>
