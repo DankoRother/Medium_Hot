5755de2a2379f994499dac4507532c0103793640
DankoRother <131434063+DankoRother@users.noreply.github.com>
2023-12-21 23:47:20 +0100

    Tatsächlich letzte Changes, war schön mit euch Jungs

--- PHP_Funktionen/editAccount.php
+++ PHP_Funktionen/editAccount.php
@@ -13,40 +13,47 @@ if (isset($_SESSION['logged_in_userID'])) {
     // Retrieve the field and value from the AJAX request
     $field = $_POST['field'];
     $value = $_POST['value'];
-
+    $passwordValidation = $_POST['validationPassword'];
     // Hash the password if the field is 'password'
     if($field == 'password') {
         $value = password_hash($value, PASSWORD_DEFAULT);
     }
-
-    // Check if the new value already exists for email or username
-    if (($field === "email" || $field === "username") && valueExists($conn, $field, $value)) {
-        // Inform the user if the value already exists
-        if($field === "email") {
-            echo("Diese E-Mail ist bereits vergeben!");
-        }
-        if($field === "username"){
-            echo("Dieser Username ist bereits vergeben!");
-        }
-    } else {
-        // Update the user table in the database
-        $query = "UPDATE user SET $field = :value WHERE userId = :userID";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':value', $value);
-        $stmt->bindParam(':userID', $userID);
-
-        // Execute the query
-        if ($stmt->execute()) {
-            echo "Data updated successfully";
+    $queryForValidation = $conn->prepare("SELECT password FROM user WHERE userId = :UserId");
+    $queryForValidation->bindValue(':UserId', $userID);
+    $queryForValidation->execute();
+    // Fetch the row as an associative array
+    $userPassword = $queryForValidation->fetchColumn();
+    if (password_verify($passwordValidation, $userPassword)) {
+        // Check if the new value already exists for email or username
+        if (($field === "email" || $field === "username") && valueExists($conn, $field, $value)) {
+            // Inform the user if the value already exists
+            if($field === "email") {
+                echo("Diese E-Mail ist bereits vergeben!");
+            }
+            if($field === "username"){
+                echo("Dieser Username ist bereits vergeben!");
+            }
         } else {
-            echo "Error updating data";
+            // Update the user table in the database
+            $query = "UPDATE user SET $field = :value WHERE userId = :userID";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':value', $value);
+            $stmt->bindParam(':userID', $userID);
+
+            // Execute the query
+            if ($stmt->execute()) {
+                echo "Data updated successfully";
+            } else {
+                echo "Error updating data";
+            }
         }
-    }
+} else {
+    echo "Falsches Passwort!";
+}
 } else {
     // Inform the user if not logged in
     echo "User not logged in";
 }
-
 // Function to check if the value already exists in the database for email or username
 function valueExists($conn, $field, $value) {
     try {
