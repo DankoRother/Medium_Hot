f32f6739cc720cb6445123710b968ac55ceebedf
DankoRother <131434063+DankoRother@users.noreply.github.com>
2023-12-21 11:54:43 +0100

    Statische Seiten einheitlich formatiert

--- datenschutz.php
+++ datenschutz.php
@@ -19,38 +19,39 @@ $_SESSION['selected_car_id'] = 0; ?>
        <!-- <td width="10%" style="background-color: #ffffff;"></td> -->
         <td width="50%" style="background-color: #24252a;">
             <div align="center" class="dividiediv">
-                <h1>Datenschutz</h1><br>
-                <p>Die Autovermietung "CarSBA" nimmt den Schutz Ihrer personenbezogenen Daten ernst. 
-                Nachfolgend möchten wir Sie darüber informieren, welche Daten wir erheben, wie wir sie verwenden und schützen.</p><br>
-                <h3>1. Erhebung und Verarbeitung personenbezogener Daten</h3><br>
-                <p>Wir erheben und verarbeiten personenbezogene Daten, um Ihnen unsere Fahrzeuge zur Verfügung zu stellen. 
-                Dazu gehören Informationen wie Ihr Name, Ihre Adresse, Kontaktdaten, Führerscheininformationen und Zahlungsinformationen.</p><br>
-                <h3>2. Nutzung personenbezogener Daten</h3><br>
-                <p>Die von uns erfassten Daten dienen der Vertragsabwicklung, der Bereitstellung von Fahrzeugen und der Abrechnung. 
+                <h1 style="text-align: center">Datenschutz</h1><br><br>
+                <h3 style="text-align: block">Die Autovermietung "CarSBA" nimmt den Schutz Ihrer personenbezogenen Daten ernst. 
+                Nachfolgend möchten wir Sie darüber informieren, welche Daten wir erheben, wie wir sie verwenden und schützen.</h3><br><br>
+                <h2>1. Erhebung und Verarbeitung personenbezogener Daten</h2>
+                <h3>Wir erheben und verarbeiten personenbezogene Daten, um Ihnen unsere Fahrzeuge zur Verfügung zu stellen. 
+                Dazu gehören Informationen wie Ihr Name, Ihre Adresse, Kontaktdaten, Führerscheininformationen und Zahlungsinformationen.</h3><br>
+                <h2>2. Nutzung personenbezogener Daten</h2>
+                <h3>Die von uns erfassten Daten dienen der Vertragsabwicklung, der Bereitstellung von Fahrzeugen und der Abrechnung. 
                     Gelegentlich nutzen wir diese Informationen auch für Marketingzwecke, um Sie über Angebote und Dienstleistungen zu informieren. 
-                    Sie haben das Recht, dieser Verwendung Ihrer Daten zu widersprechen.</p><br>
-                <h3>3. Weitergabe von Daten an Dritte</h3><br>
-                <p>Personenbezogene Daten werden nur an Dritte weitergegeben, wenn dies zur Vertragserfüllung erforderlich ist oder gesetzlich vorgeschrieben ist. 
-                    Dritte, die Zugang zu Ihren Daten erhalten, sind verpflichtet, die Datenschutzbestimmungen einzuhalten.</p><br>
-                <h3>4. Datensicherheit</h3><br>
-                <p>Wir setzen angemessene technische und organisatorische Maßnahmen ein, um die Sicherheit Ihrer Daten zu gewährleisten. 
-                    Kundenkonten sind durch Passwörter geschützt, und Zahlungsinformationen werden verschlüsselt übertragen.</p><br>
-                <h3>5. Auskunftsrecht und Berichtigung</h3><br>
-                <p>Sie haben das Recht, Auskunft über die von uns gespeicherten Daten zu erhalten. 
-                    Falls Daten unrichtig oder unvollständig sind, werden wir diese auf Ihre Anfrage hin korrigieren oder löschen.</p><br>
-                <h3>6. Cookies und Tracking-Technologien</h3><br>
-                <p>Wir verwenden Cookies und ähnliche Technologien, um die Nutzung unserer Website zu analysieren und Ihr Benutzererlebnis zu verbessern. 
-                    Sie können die Verwendung von Cookies in Ihren Browsereinstellungen kontrollieren.</p><br>
-                <h3>7. Änderungen der Datenschutzerklärung</h3><br>
-                <p>Wir behalten uns das Recht vor, diese Datenschutzerklärung bei Bedarf zu aktualisieren. Wesentliche Änderungen werden wir Ihnen mitteilen.</p><br>
-                <h3>8. Kontaktinformationen</h3><br>
-                <p>Für Fragen zur Datenschutzerklärung oder zur Ausübung Ihrer Datenschutzrechte können Sie sich an uns wenden:</p><br>
-                <p>CarSBA GmbH<br>Willy-Brandt-Straße 75<br>20459 Hamburg<br>Kontakt: info@carsba.de</p>
+                    Sie haben das Recht, dieser Verwendung Ihrer Daten zu widersprechen.</h3><br>
+                <h2>3. Weitergabe von Daten an Dritte</h2>
+                <h3>Personenbezogene Daten werden nur an Dritte weitergegeben, wenn dies zur Vertragserfüllung erforderlich ist oder gesetzlich vorgeschrieben ist. 
+                    Dritte, die Zugang zu Ihren Daten erhalten, sind verpflichtet, die Datenschutzbestimmungen einzuhalten.</h3><br>
+                <h2>4. Datensicherheit</h2>
+                <h3>Wir setzen angemessene technische und organisatorische Maßnahmen ein, um die Sicherheit Ihrer Daten zu gewährleisten. 
+                    Kundenkonten sind durch Passwörter geschützt, und Zahlungsinformationen werden verschlüsselt übertragen.</h3><br>
+                <h2>5. Auskunftsrecht und Berichtigung</h2>
+                <h3>Sie haben das Recht, Auskunft über die von uns gespeicherten Daten zu erhalten. 
+                    Falls Daten unrichtig oder unvollständig sind, werden wir diese auf Ihre Anfrage hin korrigieren oder löschen.</h3><br>
+                <h2>6. Cookies und Tracking-Technologien</h2>
+                <h3>Wir verwenden Cookies und ähnliche Technologien, um die Nutzung unserer Website zu analysieren und Ihr Benutzererlebnis zu verbessern. 
+                    Sie können die Verwendung von Cookies in Ihren Browsereinstellungen kontrollieren.</h3><br>
+                <h2>7. Änderungen der Datenschutzerklärung</h2>
+                <h3>Wir behalten uns das Recht vor, diese Datenschutzerklärung bei Bedarf zu aktualisieren. Wesentliche Änderungen werden wir Ihnen mitteilen.</h3><br>
+                <h2>8. Kontaktinformationen</h2>
+                <h3>Für Fragen zur Datenschutzerklärung oder zur Ausübung Ihrer Datenschutzrechte können Sie sich an uns wenden:</h3><br>
+                <h3>CarSBA GmbH<br>Willy-Brandt-Straße 75<br>20459 Hamburg<br>Kontakt: info@carsba.de</h3>
             </div>
         </td>
            <!-- <td width="10%" style="background-color: #ffffff;"></td>-->
     </tr>
 </table>
+
 <?php include 'footer.php'; ?>      
 </body>
 
